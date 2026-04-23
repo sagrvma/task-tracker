2dfd3114c342ae9203a7b7101c4121aae1296c53
sagrvma <notsagarverma@gmail.com>
2026-04-23 18:52:14 +0530

    added POST method to add task to the task list

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+tasks = [
+    {"id": 1, "title": "Basic initial setup", "done": False},
+    {"id": 2, "title": "Build Task Tracker", "done": False},
+    {"id": 3, "title": "Connect with React", "done": False}
+]
+
 @app.route("/api/health")
 def health():
-    return jsonify({
-        "status": "ok"
-    })
+    return jsonify(tasks)
+
+@app.route("/api/tasks", methods=["POST"])
+def add_task():
+    data = request.get_json()
+    new_task = {
+        "id": len(tasks)+1,
+        "title": data["title"],
+        "done": False
+    }
+    tasks.append(new_task)
+    return jsonify(tasks), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
